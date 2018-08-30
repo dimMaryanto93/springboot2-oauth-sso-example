fbe6fb5d657f59faacffaa71e669373f8bc411af
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-08-31 04:59:17 +0700

    init project spring boot v2

---
+++ src/main/java/com/maryanto/dimas/example/Oauth2Sso.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Sso {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Sso.class, args);
+	}
+}
